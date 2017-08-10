f050c4047b16dc354f61c9b0b60000abdcf0bfdb
Tseten Lama <tdl2116@columbia.edu>
2017-08-10 11:40:27 -0400

    Create base fixed header table with styling

---
+++ js/main.js
@@ -0,0 +1,11 @@
+for(let i = 0; i < 30; i++) {
+    $("#body").append(`
+        <tr>
+        <td>Item 1</td>
+        <td>Item 2</td>
+        <td>Item 3</td>
+        <td>Item 4</td>
+        <td>Item 5</td>
+        </tr>
+        `);
+};
